68a6564a42d768b72f44b01bf700cc5503b25162
vagrant <vagrant@ironhack>
2014-03-26 09:25:20 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cvpolitics::Application.config.secret_key_base = '0fa0cb80f714a8d7343c92fec618af469380a868031bb9771abd6c7d8bf456bceba2785fffdc8750c7fb86e9fefcbd505c1276c56a2ecf7492ee909460ebff79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cvpolitics::Application.config.session_store :cookie_store, key: '_cvpolitics_session'
