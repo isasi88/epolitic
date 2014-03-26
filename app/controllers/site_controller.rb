6d53ad5eae07f9f65d815fab2c2c8635718eb285
vagrant <vagrant@ironhack>
2014-03-26 09:48:16 +0000

    static pages

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,9 @@
+class SiteController < ApplicationController
+
+	def home
+	end
+
+	def about_us
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Cvpolitics::Application.routes.draw do
+
+  get '/' => 'site#home'
+  get('/about_us': 'site#about_us')
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
