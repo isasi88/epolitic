3ab39d1866ca5715db220ed0452f5c3831bb7b79
vagrant <vagrant@ironhack>
2014-03-29 16:31:39 +0000

    last few changes

--- db/migrate/20140326100637_create_polititians.rb
+++ db/migrate/20140326100637_create_polititians.rb
@@ -1,16 +1,30 @@
 class CreatePolititians < ActiveRecord::Migration
   def change
     create_table :polititians do |t|
-    	t.string :name
+  	  	t.string :name
     	t.string :last_name
-    	t.text :position
-    	t.date :position_start_at
-    	t.date :position_end_at
-    	t.boolean :university_education
-    	t.boolean :prev_prof_experience
+    	t.string :political_position
+    	t.date :political_position_start_at
+    	t.date :political_position_end_at
+    	t.boolean :higher_education
+    	t.string :higher_education_center
+    	t.string :higher_education_degree
+    	t.date :higher_education_start_at
+    	t.date :higher_education_end_at
+    	t.boolean :prof_experience
+    	t.string :prof_experience_employer
+    	t.string :prof_experience_position
+    	t.date :prof_expererience_start_at
+    	t.date :prof_experience_end_at
     	t.boolean :public_exam
-    	t.boolean :knows_english
-    	t.boolean :corruption_impeach    	
+    	t.string :public_exam_title
+    	t.date :public_exam_date
+    	t.boolean :english
+    	t.string :english_level
+    	t.boolean :corruption
+    	t.string :corruption_title
+    	t.string :corruption_description
+    	t.date :impeach_date
       	t.timestamps
     end
   end
