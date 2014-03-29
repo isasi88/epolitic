1df048f42c6d2a62cc22c3cd595dd3875ea75e1d
vagrant <vagrant@ironhack>
2014-03-29 18:42:04 +0000

    method new for polititians_controller, new view formatted with bootstrap

--- app/controllers/polititians_controller.rb
+++ app/controllers/polititians_controller.rb
@@ -12,4 +12,16 @@ class PolititiansController < ApplicationController
 		@polititian = Polititian.new
 	end
 
+	def create
+		@polititian = Polititian.new(entry_params)
+		@polititian.save
+		render 'show'
+	end
+
+
+
+	def entry_params
+		params.require(:polititian).permit(:name, :last_name, :political_position, :political_position_start_at, :political_position_end_at)
+	end
+
 end
