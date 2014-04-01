b94c8267a070a2a6fbef8165477eaceea35bae47
vagrant <vagrant@ironhack>
2014-04-01 09:54:13 +0000

    education working almost fine2

---
+++ app/controllers/educations_controller.rb
@@ -0,0 +1,41 @@
+class EducationsController < ApplicationController
+
+	def create
+		@polititian = Polititian.find params[:polititian_id]
+		@education = @polititian.educations.build entry_params
+		if @education.save
+			flash[:success] = "Education created succesfully"
+			redirect_to action: 'show', controller: 'polititians', id: @polititian.id
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@education = Education.find params[:id]
+		@polititian = Polititian.find params[:polititian_id]		
+	end
+
+	def update
+		@education = Education.find params[:id]
+		@polititian = Polititian.find params[:polititian_id]
+		if @education.update_attributes entry_params
+			redirect_to action: 'show', controller: 'polititians', id: @polititian.id
+			flash[:success] = "Education updated succesfully"
+		else
+			render 'edit'
+		end	
+	end
+
+	def destroy
+		@education = Education.find params[:id]
+		@polititian = Polititian.find params[:polititian_id]
+		@education.destroy
+		redirect_to action: 'show', controller: 'polititians', id: @polititian.id
+	end
+
+	def entry_params
+		params.require(:education).permit(:employer, :position, :description, :start_at, :end_at)
+	end
+
+end
