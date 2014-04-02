54955080470814cb1df65a8a4ed66a2971dfb9d5
vagrant <vagrant@ironhack>
2014-04-02 09:49:44 +0000

    icons working fine

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,7 @@
 class SiteController < ApplicationController
 
 	def home
-		render 'home'
+		render 'home', layout: 'home'
 	end
 
 	def about_us
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,29 @@ module ApplicationHelper
 			nil
 		end
 	end
+
+	def check_corruption(polititian)
+		if polititian.trials.count == 0
+			'<div class="icon-ok"><span class="glyphicon glyphicon-ok"></span></div>'.html_safe
+		else
+			'<div class="icon-bad"><span class="glyphicon glyphicon-warning-sign"></span></div>'.html_safe
+		end
+	end	
+
+	def check_education(polititian)
+		if polititian.educations.count == 0
+			'<div class="icon-bad"><span class="glyphicon glyphicon-warning-sign"></span></div>'.html_safe
+		else
+			'<div class="icon-ok"><span class="glyphicon glyphicon-ok"></span></div>'.html_safe
+		end
+	end
+
+	def check_job(polititian)
+		if polititian.jobs.count == 0
+			'<div class="icon-bad"><span class="glyphicon glyphicon-warning-sign"></span></div>'.html_safe
+		else
+			'<div class="icon-ok"><span class="glyphicon glyphicon-ok"></span></div>'.html_safe
+		end
+	end
+
 end
