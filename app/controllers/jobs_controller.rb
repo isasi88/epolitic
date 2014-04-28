793d081078337cd9976dba1976f2383961619d64
tabivicuna <tabivicuna@gmail.com>
2014-04-28 14:14:08 +0200

    changes to politician profile5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -35,7 +35,7 @@ class JobsController < ApplicationController
 	end
 
 	def entry_params
-		params.require(:job).permit(:employer, :position, :political_employer, :description, :start_at, :end_at, :currently_work_here)
+		params.require(:job).permit(:employer, :position, :political_employer, :description, :start_at, :end_at, :currently_work_here, :salary)
 	end
 
 end
