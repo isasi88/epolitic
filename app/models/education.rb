7b5b0d97c56ddfba7270927b1f9fa314e2313e61
tabivicuna <tabivicuna@gmail.com>
2014-04-29 13:06:58 +0200

    changes to politician profile7

--- app/models/education.rb
+++ app/models/education.rb
@@ -2,4 +2,8 @@ class Education < ActiveRecord::Base
 	belongs_to :polititian
 	validates :degree, presence: true
 	validates :polititian, presence: true 
+
+	scope :finished_educations, -> { where(not_finished: false) }
+	scope :non_finished_educations, -> { where(not_finished: true) }
+
 end
