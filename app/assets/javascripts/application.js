153dd8978e0ab22befe76020f0457ebd5e96c2f5
tabivicuna <tabivicuna@gmail.com>
2014-05-12 17:17:45 +0200

    added dropdown for action in index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require bootstrap
+//= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require_tree .
