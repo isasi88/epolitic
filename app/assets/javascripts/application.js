3848e040f2c8d4e9885e28087239a4a1022873cc
tabivicuna <tabivicuna@gmail.com>
2014-05-13 11:08:05 +0200

    removed a file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
-//= require jquery_ujs
 //= require jquery.ui.all
+//= require jquery_ujs
+//= require bootstrap
 //= require_tree .
-
 
 
 
