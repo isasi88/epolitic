ffa5461d582fef1fd1a14541407667f9609a0f84
tabivicuna <tabivicuna@gmail.com>
2014-05-13 09:13:23 +0200

    added new default pics

--- app/uploaders/group_avatar_uploader.rb
+++ app/uploaders/group_avatar_uploader.rb
@@ -18,7 +18,7 @@ class GroupAvatarUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
    def default_url
-    "fallback/default.png"
+    "fallback/default2.gif"
 
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
