04c6e112ef3b32979f955e45d4789018847d19b7
tabivicuna <tabivicuna@gmail.com>
2014-05-19 10:23:54 +0200

    changes to have s3 working only in production

--- app/uploaders/polititian_avatar_uploader.rb
+++ app/uploaders/polititian_avatar_uploader.rb
@@ -8,9 +8,9 @@ class PolititianAvatarUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
-  include CarrierWave::MimeTypes
-  process :set_content_type
+  # storage :fog
+  # include CarrierWave::MimeTypes
+  # process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,16 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
+
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AKIAJUYWIUY3GHBHVKLA',                        # required
     :aws_secret_access_key  => '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',                        # required
     :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'epolitic'                     # required
+    }
+    config.fog_directory = 'epolitic'
+  end
+
 end
