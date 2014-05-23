9aa4f7aabba0bb8c270728d0fc2a5758d5d335c9
Enrique Isasi <kikeisasi@gmail.com>
2014-05-23 02:10:34 +0200

    Create seed data and add images

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,36 @@
 #50.times do |i|
 #	Polititian.create name: "Nombre#{i}", last_name:"Apellido#{i}"
 #end
+  # Add groups to iterate
+  GROUPS = ["Partido Popular","PSOE","Podemos","IU","EH Bildu","Los Verdes","Antitaurino"]
 
-Group.create(name: "Partido Popular")
-Group.create(name: "PSOE")
+  # We create an image rotation for politician group
+group_image = lambda {
+ @images ||= begin
+   Dir[File.expand_path('../images/groups/*', __FILE__)]
+ end
+ @images.rotate!.first
+}
+
+  # Generate Groups
+  GROUPS.each do |g|
+    Group.create(name: g, description: Faker::Lorem.sentence(1), avatar: File.open(group_image.call))
+  puts "Groups Created"
+  end 
+
+    # We create an image rotation for politician group
+polititian_image = lambda {
+ @p_images ||= begin
+   Dir[File.expand_path('../images/polititians/*', __FILE__)]
+ end
+ @p_images.rotate!.first
+}
+
+
+    # Generate Polititians
+  30.times do |p|
+    Polititian.create!(first_name: Faker::Name.name, last_name: Faker::Name.name, group_id: rand(1..5), avatar: File.open(polititian_image.call))
+  puts "Politician Created"
+  end 
+
 
